51724bb664f5dcb070a749c72e8adb78ee6733f7
LokeshReddy Pothapu <lokesh@LokeshReddys-MacBook-Pro.local>
2017-12-10 09:49:51 -0800

    first commit

--- experiment/ViewController.swift
+++ experiment/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var expButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func expBTN(_ sender: Any) {
+        expButton.backgroundColor = UIColor.red
+    }
+    
 
 }
 
